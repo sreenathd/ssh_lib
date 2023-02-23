5b09b4366ed16facb04cc6a762442c7818cb46c6
Sreenath D <sreenathd2002@gmail.com>
2023-02-23 08:08:15 +0530

    Update ssh_over_pexpect.py

--- ssh_over_pexpect.py
+++ ssh_over_pexpect.py
@@ -1,19 +1,36 @@
 import pexpect
 
-def tries(c):
-    for tries in range(5):
-        c.send('\r')
-        cli = c.expect([pexpect.TIMEOUT, '$ ' , '*~$ '], timeout=1)
-        if cli!=0: 
-            break
+class sshPexpect:
+    def __init__(self,username, passwd, servername, logfile = "/tmp/mylog" ):
+        self.username = username
+        self.password = passwd
+        self.servername = servername
+        self.log_file = open(logfile, "bw")
+    
+    def tries(self,c):
+        for tries in range(5):
+            c.send('\r')
+            cli = c.expect([pexpect.TIMEOUT, '$ ' , '*~$ '], timeout=1)
+            if cli!=0: 
+                break
+                
+    def connect(self):
+        self.c = pexpect.spawn('/usr/bin/ssh ' + self.username+ '@' + self.servername, timeout=120)
+        self.c.logfile = self.log_file
+        self.c.expect('password:', timeout=120)
+        self.c.sendline(self.passwd)
+        tries(self.c)
+    
+    def send_cmd(self, cmd):
+        self.c.sendline(cmd)
+        tries(self.c)
+        
+    def close_session(self):
+        self.c.close()
+        self.log_file.close()
 
-c = pexpect.spawn('/usr/bin/ssh sreenath@127.0.0.1', timeout=120)
-fout = open("/tmp/mylog", "bw")
-c.logfile = fout
-c.expect('password:', timeout=120)
-c.sendline('siddharth')
-tries(c)
-#print(c.before)
-c.sendline('ls -latr')
-tries(c)
-fout.close()
+if __name__ == '__main__':
+    sh_shell = sshPexpect('user','pswd','127.0.0.1')
+    sh_shell.connect()
+    sh_shell.send_cmd('ls -latr')
+    
