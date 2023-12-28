84cb32bde5870c1d6ce2bea770c733c1eeac089b
Sreenath <sreeanthd2002@gmail.com>
2023-12-28 03:50:56 +0000

    outl

---
+++ outl.py
@@ -0,0 +1,3 @@
+user_list = ['Dian Jin', 'Ian (Tom) Tong Pan', 'Meng Zhou', 'Suraj Anand', 'Hazel Ma', 'Png Chen Wei', 'Ryan Wong', 'Aansh Raiyani', 'Aishani Shukla', 'Alex Ori', 'Ariel Wang', 'Claire Macedonia', 'Lucas Balon', 'Ryan Young', 'Taylor Kiefer', 'Amy Yang', 'Dhruv Mathur', 'Jasmine Filawo', 'Jonas Meltzer', 'Selin Irmak Koksal', 'Travis Green', 'Auras Khanal', 'Lucy Sun', 'Neil Kachappilly', 'Rohit Chougule', 'Gracjan Pawlukiewicz', 'Mateusz Borowski', 'Kinga Franczak', 'Yunus Karazak', 'Corinne Blaise', 'Jotham Kriakos', 'Manas Ramech']
+for item in user_list:
+    
